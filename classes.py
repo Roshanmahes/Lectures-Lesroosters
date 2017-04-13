ef39f466e2b921fbbecb16eb184bfaff6344cea3
Gido Limperg <glimperg@live.nl>
2017-04-13 12:24:37 +0200

    More small changes to classes.py
    Translated variable names into English

--- classes.py
+++ classes.py
@@ -15,28 +15,28 @@ class Course:
     """
     def __init__(self, data, students):
         self.name = data[0]
-        self.hoorcolleges = int(data[1])
-        self.werkcolleges = int(data[2])
-        self.wc_cap = int(data[3])
-        self.practica = int(data[4])
+        self.lectures = int(data[1])
+        self.seminars = int(data[2])
+        self.s_cap = int(data[3])
+        self.practicals = int(data[4])
         self.p_cap = int(data[5])
         self.students = students
 
-class LectureHall:
+"""class TeachingHall:
     def __init__(self, data):
         self.name = data[0]
         self.cap = int(data[1])
+"""
 
-class Lecture:
-    def __init__(self, _type, course, students, lecturehall):
+class Teaching:
+    def __init__(self, _type, course, students):
         self.courseName = course.name
         self.type = _type
-        # studenten wisselen tussen lecture classes met dezelfde courseName en type
-        if _type == "hoorcollege":
+        # studenten wisselen tussen teaching classes met dezelfde courseName en type
+        if _type == "lecture":
             self.cap = len(students)
-        elif _type == "werkcollege":
-            self.cap = course.wc_cap
+        elif _type == "seminar":
+            self.cap = course.s_cap
         else:
             self.cap = course.p_cap
         self.students = students
-        self.lecturehall = lecturehall
