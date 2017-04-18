638c7edd8937cc5839251d3ce5c1fe47782fe179
Philip Roeleveld <phi_lip@live.nl>
2017-04-18 16:29:21 +0200

    Tidying up state space calculations

---
+++ state_space.py
@@ -0,0 +1,36 @@
+def calculate(courses, teachings, hall_list):
+    """
+    Calculates an upper bound to the number
+    of configurations of the state space
+    """
+
+    # upper bound to number of ways the teachings can be scheduled
+    prod1 = 1
+    tracker = 0
+    teaching_sizes = [len(teaching.students) for teaching in teachings]
+    teaching_sizes.sort(reverse=True)
+    for size in teaching_sizes:
+        big_enough_halls = 0
+        for hall in hall_list:
+            if size < int(hall[1]):
+                big_enough_halls += 1
+        prod1 *= big_enough_halls * 20 - tracker
+        tracker += 1
+
+    # upper bound to number of ways
+    # the students can be put into the S and P groups
+    prod2 = 1
+    for course in courses:
+        if course.seminars:
+            group_count = course.get_group_count("seminar")
+            capacity = course.s_cap
+        elif  course.practicals:
+            group_count = course.get_group_count("practical")
+            capacity = course.p_cap
+        else:
+            group_count = 0
+        if group_count:
+            prod2 *= (group_count ** len(course.students)) \
+                    ** (course.seminars+course.practicals)
+
+    return prod1*prod2
