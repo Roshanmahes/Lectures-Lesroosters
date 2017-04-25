83002590902d5e5b1239247bb051355a04478b4f
Philip Roeleveld <phi_lip@live.nl>
2017-04-25 11:03:07 +0200

    Changed state_space with the fixes discussed during presentation, added comments for clarity

--- state_space.py
+++ state_space.py
@@ -1,4 +1,7 @@
+from math import factorial
+
 TIMESLOTS = 20
+EVENINGSLOTS = 5
 
 def calculate(courses, teachings, hall_list):
     """
@@ -7,22 +10,15 @@ def calculate(courses, teachings, hall_list):
     """
 
     # upper bound to number of ways the teachings can be scheduled
-    prod1 = 1
-    tracker = 0
-    teaching_sizes = [len(teaching.students) for teaching in teachings]
-    teaching_sizes.sort(reverse=True)
-    for size in teaching_sizes:
-        big_enough_halls = 0
-        for hall in hall_list:
-            if size < int(hall[1]):
-                big_enough_halls += 1
-        prod1 *= big_enough_halls * TIMESLOTS - tracker
-        tracker += 1
+    slots = TIMESLOTS * len(hall_list) + EVENINGSLOTS
+    prod1 = int(factorial(slots)/factorial(slots - len(teachings)))
 
-    # upper bound to number of ways
-    # the students can be put into the S and P groups
+    # upper bound to number of ways the students can be put
+    # into the S and P groups for each course
     prod2 = 1
     for course in courses:
+        # get the capacity that is non-zero, if it exists
+        # (works because s_cap and p_cap are always equal if they are both non-zero)
         if course.seminars:
             group_count = course.get_group_count("seminar")
             capacity = course.s_cap
@@ -31,6 +27,7 @@ def calculate(courses, teachings, hall_list):
             capacity = course.p_cap
         else:
             group_count = 0
+        # if the group_count is greater than 0
         if group_count:
             prod2 *= (group_count ** len(course.students)) \
                     ** (course.seminars+course.practicals)
