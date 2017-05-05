26924766b64765b9a2abae82877f24564e0fbf0e
Roshanmahes <roshan-1@live.com>
2017-05-05 21:55:23 +0100

    UPDATE results
    Tested random_fit. New highscore: 529.

--- algorithms.py
+++ algorithms.py
@@ -3,6 +3,8 @@ import random
 
 ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 TIMESLOTS = 20
+TEACHINGS = 129
+HALLS = 7
 
 def alphabetical(courses, halls):
     """
@@ -61,7 +63,8 @@ def alphabetical(courses, halls):
 
 def random_walk(courses, halls):
     """
-
+    Creates a schedule, filling all halls with teachings randomly.
+    Returns a list of lists containing Teaching objects.
     """
     teachings = []
 
@@ -111,5 +114,83 @@ def random_walk(courses, halls):
         teaching.hall = halls[rand // 20]
         schedule[rand // 20][rand % 20] = teaching
         entries.remove(rand)
+
+    return schedule
+
+def random_fit(courses, halls):
+    """
+    Creates a schedule, filling all halls with teachings
+    randomly provided that the hall is the first hall
+    which is available andlarge enough.
+    Returns a list of lists containing Teaching objects.
+    """
+    teachings = []
+
+    for course in courses:
+        for _ in range(course.lectures):
+            # assign students to lecture
+            teachings.append(classes.Teaching("lecture",
+                    course, course.students))
+
+        if course.seminars:
+            group_count = course.get_group_count("seminar")
+            students_per_group = [0] * group_count
+
+            seminars = [classes.Teaching("seminar", course, [], ALPHABET[i]) for i in range(group_count)]
+
+            for student in course.students:
+                rand = random.randint(0, group_count - 1)
+                while students_per_group[rand] == course.s_cap:
+                    rand = random.randint(0, group_count - 1)
+                seminars[rand].students.append(student)
+                students_per_group[rand] += 1
+
+            for seminar in seminars:
+                teachings.append(seminar)
+
+        if course.practicals:
+            group_count = course.get_group_count("practical")
+            students_per_group = [0] * group_count
+
+            practicals = [classes.Teaching("practical", course, [], ALPHABET[i]) for i in range(group_count)]
+
+            for student in course.students:
+                rand = random.randint(0, group_count - 1)
+                while students_per_group[rand] == course.p_cap:
+                    rand = random.randint(0, group_count - 1)
+                practicals[rand].students.append(student)
+                students_per_group[rand] += 1
+
+            for practical in practicals:
+                teachings.append(practical)
+
+    # create an empty schedule of the right dimensions
+    schedule = [[None for i in range(TIMESLOTS)] for j in range(len(halls))]
+
+    # keep track of how many timeslots have been filled for each hall
+    tracker = [0] * len(halls)
+
+    teaching_index_list = random.sample(list(range(TEACHINGS)), TEACHINGS)
+
+    for index in teaching_index_list:
+
+        teaching = teachings[index]
+
+        for i,hall in enumerate(halls):
+
+            # if the amount of students fits into the hall
+            if len(teaching.students) <= hall.capacity:
+
+                # if less than all slots have been filled
+                if tracker[i] < TIMESLOTS:
+
+                    # insert the teaching into the schedule
+                    schedule[i][tracker[i]] = teaching
+
+                    # add hall and timeslot to teaching object
+                    teaching.hall = halls[i]
+                    teaching.timeslot = tracker[i]
+                    tracker[i] += 1
+                    break
 
     return schedule
--- main.py
+++ main.py
@@ -26,13 +26,13 @@ def  main():
     # create list of Course objects
     courses = create_course_list(course_list, students)
 
-<<<<<<< HEAD
-    # create a simple schedule
+    # show some facts about the random_scores.csv file which contains the
+    # scores of 10 million randomly generated schedules
     #score_list = []
-    #best_score = -300
-    #worst_score = -300
+    #best_score = 0
+    #worst_score = 1000
     #for _ in range(1000000):
-    schedule = algorithms.random_walk(courses, halls)
+    schedule = algorithms.alphabetical(courses, halls)
     points = score(schedule, courses)
     #    score_list.append(points)
     #    if points >= best_score:
@@ -41,27 +41,6 @@ def  main():
     #    elif points < worst_score:
     #        worst_score = points
     #        worst_schedule = schedule
-
-    print_schedule(halls, schedule)
-    #print(best_score, worst_score)
-    #print_schedule(halls, best_schedule)
-    #print_schedule(halls, worst_schedule)
-    print("\nScore:", points)
-=======
-    # show some facts about the random_scores.csv file which contains the
-    # scores of 10 million randomly generated schedules
-    scores = []
-    with open("data/random_scores.csv") as f:
-        reader = csv.reader(f)
-        for score in next(reader):
-            if score:
-                scores.append(int(score))
-    scores.sort()
-    print(scores[:10])
-    print(scores[-10:])
-    print(sum(scores)/len(scores))
-
->>>>>>> origin/master
 
 if __name__ == "__main__":
     main()
